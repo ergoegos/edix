27bb70166389286f0816264207bc2bd337ba06bb
ergoegos <ergoegos@gmail.com>
2022-12-08 23:59:34 +0100

    hello autowired

--- CFGS DAM/MP_0490 Programación de servicios y procesos/AR. Servidor RESTful buscador de personas/person/src/main/java/rest/ful/person/controller/PersonController.java
+++ CFGS DAM/MP_0490 Programación de servicios y procesos/AR. Servidor RESTful buscador de personas/person/src/main/java/rest/ful/person/controller/PersonController.java
@@ -1,8 +1,13 @@
 package rest.ful.person.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import rest.ful.person.entity.Person;
 
 @Controller
 public class PersonController {
 	
+	@Autowired
+	Person person;
 }
